24ff1fbb6cb5d5a98c2735cb616c5e93e794fe09
Bojidar00 <bozhidar.rusalinov@quanterall.com>
2022-09-19 16:49:58 +0300

    feat: Add and remove member from event

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import {uploadDataToIpfs , fetchEventsMetadata , deleteDataFromService} from "./src/utils/ipfs.utils.js"
-import { eventTicketingSystemContract, checkRemoveTransaction} from './src/configs/contract.config.js';
+import { eventTicketingSystemContract,provider,EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS,ABI} from './src/configs/contract.config.js';
+import { ethers } from "ethers";
 
-export async function createEvent(apiKey, metadata, image, maxTicketPerClient) {
+
+
+async function checkRemoveTransaction(address, eventId){
+  let signer = new ethers.VoidSigner(address, provider);
+  let contract = new ethers.Contract(  EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS, ABI, signer);
+  let result = await contract.callStatic.removeEvent(eventId);
+  return result;
+}
+
+async function checkUpdateTransaction(address, eventId){
+  let signer = new ethers.VoidSigner(address, provider);
+  let contract = new ethers.Contract(  EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS, ABI, signer);
+  let result = await contract.callStatic.updateEventTokenUri(eventId,"");
+  return result;
+}
+
+export async function createEvent(nftStorageApiKey, metadata, image, maxTicketPerClient) {
     try {
       const url = await uploadDataToIpfs(
-       apiKey,
+       nftStorageApiKey,
        metadata,
        image
       );
@@ -29,6 +46,18 @@ export async function fetchEvents(eventIds) {
   }
 }
 
+export async function fetchOwnedEvents(address){
+  let signer = new ethers.VoidSigner(address, provider);
+  const contract = new ethers.Contract(
+    EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS,
+    ABI,
+    signer
+  );
+  let eventIds = await contract.fetchOwnedEvents();
+  let events = await fetchEvents(eventIds);
+  return events;
+}
+
 export async function removeEvent(nftStorageApiKey, eventId, address){
 
   try {
@@ -41,4 +70,43 @@ return tx;
   console.error(`Error: ${error}`);
 }
 
+}
+
+export async function updateEvent(nftStorageApiKey, eventId, metadata,image,address){
+  try {
+    let result =await checkUpdateTransaction(address, eventId);
+    await deleteDataFromService(nftStorageApiKey, eventId);
+    const url = await uploadDataToIpfs(
+      nftStorageApiKey,
+      metadata,
+      image
+     );
+     let tx = await eventTicketingSystemContract.populateTransaction.updateEventTokenUri(eventId,url);
+     return tx;
+  }
+  catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+
+export async function addTeamMember(eventId, role, address){
+try{
+let tx = await eventTicketingSystemContract.populateTransaction.addTeamMember(eventId,role,address);
+return tx;
+}
+catch (error) {
+  console.error(`Error: ${error}`);
+}
+
+}
+
+export async function removeTeamMember(eventId, role, address){
+  try{
+  let tx = await eventTicketingSystemContract.populateTransaction.removeTeamMember(eventId,role,address);
+  return tx;
+  }
+  catch (error) {
+    console.error(`Error: ${error}`);
+  }
+  
 }
--- src/configs/contract.config.js
+++ src/configs/contract.config.js
@@ -2,22 +2,19 @@ import { ethers } from "ethers";
 import {schema} from '../schema/EventTicketingSystem.js';
 
 const AVALANCHE_TESTNET_API = "https://api.avax-test.network/ext/bc/C/rpc";
-const EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS = "0x687F6de4D6e398aCA20836469EfB1b7AC568b7B8";
+const EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS = "0x8Dd331A2F97d0623229017cefbE210C14AB8eaf8";
 const provider = ethers.getDefaultProvider(AVALANCHE_TESTNET_API);
+const ABI = schema.abi;
 const eventTicketingSystemContract = new ethers.Contract(
   EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS,
-  schema.abi,
+  ABI,
   provider
 );
 
-async function checkRemoveTransaction(address, eventId){
-  let signer = new ethers.VoidSigner(address, provider);
-  let contract = new ethers.Contract(  EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS, schema.abi, signer);
-  let result = await contract.callStatic.removeEvent(eventId);
-  return result;
-}
 
 export {
   eventTicketingSystemContract,
-  checkRemoveTransaction,
+  provider,
+  EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS,
+  ABI
 };
