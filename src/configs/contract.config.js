dd630d618339864340d12305402a94f0e2253777
Bojidar00 <bozhidar.rusalinov@quanterall.com>
2022-09-28 11:48:50 +0300

    feat: Get constants from ets-contracts

--- src/configs/contract.config.js
+++ src/configs/contract.config.js
@@ -1,7 +1,5 @@
 import schema from "../../node_modules/event-ticketing-system/artifacts/contracts/EventTicketingSystem.sol/EventTicketingSystem.json";
-const EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS =
-  "0x8Dd331A2F97d0623229017cefbE210C14AB8eaf8";
-const AVALANCHE_TESTNET_API = "https://api.avax-test.network/ext/bc/C/rpc";
+import {EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS, AVALANCHE_TESTNET_API} from "../../node_modules/event-ticketing-system/config/index.config.js";
 const IPFS_GATEWAY_PROVIDER_URL = "https://nftstorage.link/ipfs/";
 const ABI = schema.abi;
 export {
