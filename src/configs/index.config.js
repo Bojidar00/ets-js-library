9a00fb34e832b904f3d70f3a61b4dbdf0fe9f2b3
drseu55 <alpics1337@gmail.com>
2022-10-10 10:14:03 +0300

    fix: Add ets server default value and export ipfs gateway fn

--- src/configs/index.config.js
+++ src/configs/index.config.js
@@ -3,11 +3,13 @@ import {
   EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS,
   AVALANCHE_TESTNET_API,
 } from "ets-contracts/config/index.config";
-const IPFS_GATEWAY_PROVIDER_URL = "https://nftstorage.link/ipfs/";
 const ABI = schema.abi;
+const IPFS_GATEWAY_PROVIDER_URL = "https://nftstorage.link/ipfs/";
+const ETS_SERVER_URL = "http://127.0.0.1:1337";
 export {
-  IPFS_GATEWAY_PROVIDER_URL,
   AVALANCHE_TESTNET_API,
   EVENT_TICKETING_SYSTEM_CONTRACT_ADDRESS,
   ABI,
+  IPFS_GATEWAY_PROVIDER_URL,
+  ETS_SERVER_URL,
 };
