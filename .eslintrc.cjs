97f127c070041e884c70ed01cb3f4769d732fdb8
drseu55 <alpics1337@gmail.com>
2022-10-12 13:12:15 +0300

    feat: Move event listeners from indexer

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,14 @@ module.exports = {
   },
   extends: ["standard", "plugin:jest/recommended"],
   overrides: [],
+  parser: "@babel/eslint-parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: ["@babel/plugin-syntax-import-assertions"],
+    },
   },
   ignorePatterns: ["commitlint.config.js", "coverage"],
   rules: {
