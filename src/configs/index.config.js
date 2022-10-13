c97f93580c344dddc3c326f6777d7992ae714684
Bojidar00 <bojidar_rusalinov@abv.bg>
2022-10-13 13:23:09 +0300

    feat: Optimize imports

--- src/configs/index.config.js
+++ src/configs/index.config.js
@@ -1,17 +1,23 @@
-import schema from "../../config/EventFacet.json" assert { type: "json" };
+import schema from "#contract.config/EventFacet.json" assert { type: "json" };
 import {
   EVENTS_CONTRACT_ADDRESS,
   NET_RPC_URL,
-} from "../../config/constants.cjs";
+  NET_RPC_URL_ID,
+  TOKEN_NAME,
+  NET_LABEL,
+} from "#contract.config/constants.cjs";
 
 const ABI = schema.abi;
 const IPFS_GATEWAY_PROVIDER_URL = "https://nftstorage.link/ipfs/";
 const ETS_SERVER_URL = "http://127.0.0.1:1337";
 
 export {
-  NET_RPC_URL,
-  EVENTS_CONTRACT_ADDRESS,
   ABI,
   IPFS_GATEWAY_PROVIDER_URL,
   ETS_SERVER_URL,
+  EVENTS_CONTRACT_ADDRESS,
+  NET_RPC_URL,
+  NET_RPC_URL_ID,
+  TOKEN_NAME,
+  NET_LABEL,
 };
