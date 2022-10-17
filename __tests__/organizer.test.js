6f861199e69baaf1d28b068bb4165e6b4abc4f30
ahmedovv123 <ahmedovv123@gmail.com>
2022-10-17 16:09:50 +0300

    fix: Fix tests

--- __tests__/organizer.test.js
+++ __tests__/organizer.test.js
@@ -4,70 +4,147 @@ import {
   removeTeamMember,
   updateEvent,
   fetchOwnedEvents,
+  createEvent,
+  getEventMembers,
+  getEventIpfsUri,
 } from "../src/index";
+import { ABI, NET_RPC_URL } from "#config";
 import fetch from "@web-std/fetch";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import { ethers } from "ethers";
+import fs from "fs";
 
 const NFT_STORAGE_API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY4OEYyYjcxMzhlNWUxQjJBOWQyQzc2OWREMTNBMUQwRTYyMjI5NjMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2Mjk3MzI5NzMwOSwibmFtZSI6IkV2ZW50LVRpY2tldGluZy1TeXN0ZW0ifQ.x0aXjwdGsIcxibl7eIjxhdF8vnGHB2BB-2gkScySU20";
 
+const PRIVATE_KEY = fs.readFileSync(".secret").toString().trim();
+const provider = ethers.getDefaultProvider(NET_RPC_URL);
+const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+const EVENTS_TEST_CONTRACT_ADDRESS =
+  "0xB73F457c374C7A53a80475460A11051065cBb361";
+
+const eventsTestContract = new ethers.Contract(
+  EVENTS_TEST_CONTRACT_ADDRESS,
+  ABI,
+  provider,
+);
+
 describe("Organizer tests", () => {
-  let mock;
   const address = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+  let tokenId;
 
   beforeAll(async () => {
-    mock = new MockAdapter(axios);
+    // mock = new MockAdapter(axios);
+
+    const metadata = {
+      name: "Event5",
+      description: "Event5 Description",
+      image: "null",
+      properties: {
+        eventId: "base-58 random bytes",
+        websiteUrl: "https://event1.com",
+        date: {
+          start: "2022-10-01 10:45:29.971113+03",
+          end: "2022-10-03 10:45:29.971113+03",
+        },
+        location: {
+          country: "Bulgaria",
+          city: "Razgrad",
+          address: "Front Beach Alley, 9007 Golden Sands",
+          coordinates: {
+            latitude: "43.28365485346511",
+            longitude: "28.042738484752096",
+          },
+        },
+        ticketTypes: ["super early birds", "early birds", "regular", "onsite"],
+        maxTicketsPerAccount: 10,
+        contacts: "Contacts string",
+        status: "upcoming",
+        tags: ["#programming", "#VitalikFanBoys"],
+      },
+    };
+
+    const image = await fetch(
+      "https://www.blackseachain.com/assets/img/hero-section/hero-image-compressed.png",
+    );
+    const imageBlob = await image.blob();
+
+    const populatedTx = await createEvent(
+      NFT_STORAGE_API_KEY,
+      metadata,
+      imageBlob,
+      10,
+      eventsTestContract,
+    );
+
+    const eventTx = await wallet.sendTransaction(populatedTx);
+    const eventTxResponse = await eventTx.wait();
+    const tokenIdInHex = eventTxResponse.logs[0].data.slice(2, 66);
+    tokenId = parseInt(tokenIdInHex, 16);
   });
 
   afterEach(() => {
-    mock.reset();
+    // mock.reset();
   });
 
   test("Should revert delete event when there is not an event", async () => {
+    let err;
     try {
-      await removeEvent(1);
+      const populatedTx = await removeEvent(tokenId + 1, eventsTestContract);
+      const tx = await wallet.sendTransaction(populatedTx);
+      await tx.wait();
     } catch (error) {
-      expect(error.reason).toBe("Event: Caller is not an admin"); // eslint-disable-line jest/no-conditional-expect
+      err = error;
+    } finally {
+      expect(err.error.reason).toBe(
+        "execution reverted: Event: Event does not exist!",
+      );
     }
   });
 
   test("Should call add team member method from smart contract", async () => {
     try {
-      const tx = await addTeamMember(1, `0x${"0".repeat(64)}`, address);
+      const populatedTx = await addTeamMember(
+        tokenId,
+        `0x${"0".repeat(64)}`,
+        address,
+        eventsTestContract,
+      );
+      const tx = await wallet.sendTransaction(populatedTx);
+      await tx.wait();
 
-      const functionSig = ethers.utils
-        .keccak256(
-          ethers.utils.toUtf8Bytes("addTeamMember(uint256,bytes32,address)"),
-        )
-        .slice(0, 10);
+      const eventMembers = await getEventMembers(tokenId, eventsTestContract);
 
-      // Calls the right function from smart contract
-      expect(functionSig).toBe(tx.data.slice(0, 10));
-    } catch (_error) {}
+      expect(eventMembers.length).toBe(2);
+      expect(eventMembers[1][0].toLowerCase()).toBe(address);
+    } catch (_error) {
+      console.log(_error);
+    }
   });
 
   test("Should call remove team member method from smart contract", async () => {
     try {
-      const tx = await removeTeamMember(1, `0x${"0".repeat(64)}`, address);
+      const populatedTx = await removeTeamMember(
+        tokenId,
+        `0x${"0".repeat(64)}`,
+        address,
+        eventsTestContract,
+      );
+      const tx = await wallet.sendTransaction(populatedTx);
+      await tx.wait();
 
-      const functionSig = ethers.utils
-        .keccak256(
-          ethers.utils.toUtf8Bytes("removeTeamMember(uint256,bytes32,address)"),
-        )
-        .slice(0, 10);
+      const eventMembers = await getEventMembers(tokenId);
 
-      // Calls the right function from smart contract
-      expect(functionSig).toBe(tx.data.slice(0, 10));
+      expect(eventMembers.length).toBe(1);
+      expect(eventMembers[0][0].toLowerCase()).toBe(wallet.address);
     } catch (_error) {}
   });
 
   test("Should call updateEventTokenUri method from smart contract", async () => {
+    const oldIpfsUrl = await getEventIpfsUri(tokenId, eventsTestContract);
     try {
       const metadata = {
-        name: "Event5",
-        description: "Event5 Description",
+        name: "Updated Name",
+        description: "Updated Description",
         image: "null",
         properties: {
           eventId: "base-58 random bytes",
@@ -103,35 +180,29 @@ describe("Organizer tests", () => {
       );
       const imageBlob = await image.blob();
 
-      const tx = await updateEvent(
+      const populatedTx = await updateEvent(
         NFT_STORAGE_API_KEY,
-        1,
+        tokenId,
         metadata,
         imageBlob,
-        address,
+        eventsTestContract,
       );
 
-      const functionSig = ethers.utils
-        .keccak256(
-          ethers.utils.toUtf8Bytes("updateEventTokenUri(uint256,string)"),
-        )
-        .slice(0, 10);
+      const tx = await wallet.sendTransaction(populatedTx);
+      await tx.wait();
 
-      // Calls the right function from smart contract
-      expect(functionSig).toBe(tx.data.slice(0, 10));
+      const newIpfsUrl = await getEventIpfsUri(tokenId, eventsTestContract);
+
+      expect(oldIpfsUrl.toString()).not.toBe(newIpfsUrl.toString());
     } catch (_error) {}
   });
 
   test("Should call fetchOwnedEvents from smart contract", async () => {
     try {
-      const tx = await fetchOwnedEvents();
-      const functionSig = ethers.utils
-        .keccak256(ethers.utils.toUtf8Bytes("fetchOwnedEvents"))
-        .slice(0, 10);
-
-      // Calls the right function from smart contract
-      expect(functionSig).toBe(tx.data.slice(0, 10));
-      expect(tx).toBe([]);
-    } catch (_error) {}
+      const events = await fetchOwnedEvents(wallet.address, eventsTestContract);
+      expect(events.length).toBe(tokenId);
+    } catch (_error) {
+      console.log(_error);
+    }
   });
 });
--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,20 +1,11 @@
-import {
-  createEvent,
-  fetchAllEventsFromServer,
-  fetchEvents,
-} from "../src/index";
-import fetch from "@web-std/fetch";
+import { fetchAllEventsFromServer } from "../src/index";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import { ethers } from "ethers";
-
-const NFT_STORAGE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY4OEYyYjcxMzhlNWUxQjJBOWQyQzc2OWREMTNBMUQwRTYyMjI5NjMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2Mjk3MzI5NzMwOSwibmFtZSI6IkV2ZW50LVRpY2tldGluZy1TeXN0ZW0ifQ.x0aXjwdGsIcxibl7eIjxhdF8vnGHB2BB-2gkScySU20";
 
 describe("user", () => {
   let mock;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     mock = new MockAdapter(axios);
   });
 
@@ -22,51 +13,7 @@ describe("user", () => {
     mock.reset();
   });
 
-  test("Creates event", async () => {
-    const metadata = {
-      name: "Event5",
-      description: "Event5 Description",
-      image: "null",
-      properties: {
-        eventId: "base-58 random bytes",
-        websiteUrl: "https://event1.com",
-        date: {
-          start: "2022-10-01 10:45:29.971113+03",
-          end: "2022-10-03 10:45:29.971113+03",
-        },
-        location: {
-          country: "Bulgaria",
-          city: "Razgrad",
-          address: "Front Beach Alley, 9007 Golden Sands",
-          coordinates: {
-            latitude: "43.28365485346511",
-            longitude: "28.042738484752096",
-          },
-        },
-        ticketTypes: ["super early birds", "early birds", "regular", "onsite"],
-        maxTicketsPerAccount: 10,
-        contacts: "Contacts string",
-        status: "upcoming",
-        tags: ["#programming", "#VitalikFanBoys"],
-      },
-    };
-
-    const image = await fetch(
-      "https://www.blackseachain.com/assets/img/hero-section/hero-image-compressed.png",
-    );
-    const imageBlob = await image.blob();
-
-    const tx = await createEvent(NFT_STORAGE_API_KEY, metadata, imageBlob, 10);
-    const functionSig = ethers.utils
-      .keccak256(ethers.utils.toUtf8Bytes("createEvent(uint256,string)"))
-      .slice(0, 10);
-
-    // Calls the right function from smart contract
-    expect(functionSig).toBe(tx.data.slice(0, 10));
-  });
-
   test("Gets all events from server", async () => {
-    const serverURL = "https://example-server";
     const params = {
       title: "",
       description: "",
@@ -90,51 +37,12 @@ describe("user", () => {
     };
 
     mock
-      .onPost(`${serverURL}/api/v1/events`, params)
+      .onPost()
       .reply(200, ["ipfs://metadataOfEvent1", "ipfs://metadataOfEvent2"]);
 
-    const response = await fetchAllEventsFromServer(params, serverURL);
+    const response = await fetchAllEventsFromServer(params);
     expect(response.data.toString()).toBe(
       "ipfs://metadataOfEvent1,ipfs://metadataOfEvent2",
     );
-  });
-
-  // TODO: Needs refactoring. Do not rely on contract
-  test.skip("Gets event details", async () => {
-    const metadataURI =
-      "https://nftstorage.link/ipfs/bafyreiflrftprln2zoit3lviwt63inbucb2pq6zua5hwzcqx2nnhzdfcsu/metadata.json";
-
-    const mockedData = {
-      name: "Event1",
-      description: "Event1 Description",
-      properties: {
-        eventId: "base-58 random bytes",
-        websiteUrl: "https://event1.com",
-        date: {
-          start: "2022-10-01 10:45:29.971113+03",
-          end: "2022-10-03 10:45:29.971113+03",
-        },
-        location: {
-          country: "Bulgaria",
-          city: "Razgrad",
-          address: "Front Beach Alley, 9007 Golden Sands",
-          coordinates: {
-            latitude: "43.28365485346511",
-            longitude: "28.042738484752096",
-          },
-        },
-        ticketTypes: ["super early birds", "early birds", "regular", "onsite"],
-        maxTicketsPerAccount: 10,
-        contacts: "Contacts string",
-        status: "upcoming",
-        tags: ["#programming", "#blockchain"],
-      },
-      image:
-        "ipfs://bafybeiaxbpxaxxizq2s6vvodocbo2ahvh4dbrgnrwvmw4afnjgesza22ee/blob",
-    };
-    mock.onGet(metadataURI).reply(200, mockedData);
-    const res = await fetchEvents([1]);
-
-    expect(mockedData.toString()).toBe(res[0].toString());
   });
 });
