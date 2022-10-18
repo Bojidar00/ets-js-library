d161ac207d66b155eae11f8fb0fe25c8c5e7aefe
Bojidar00 <bojidar_rusalinov@abv.bg>
2022-10-18 11:11:32 +0300

    fix: Fix tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,5 +7,6 @@ module.exports = {
     "#ipfs.utils": "<rootDir>/src/utils/ipfs.utils.js",
     "#config": "<rootDir>/src/configs/index.config.js",
     "#contract.config/(.*)$": "<rootDir>/config/$1",
+    "#contract": "<rootDir>/src/utils/contract.js",
   },
 };
