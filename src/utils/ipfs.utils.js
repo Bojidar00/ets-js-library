b80eaaa1d42b3d43fd51c46b151258eb6ebe0748
Bojidar00 <bojidar_rusalinov@abv.bg>
2022-10-19 10:28:52 +0300

    fix: Change to throw error

--- src/utils/ipfs.utils.js
+++ src/utils/ipfs.utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 import { NFTStorage } from "nft.storage";
 import axios from "axios";
 import { IPFS_GATEWAY_PROVIDER_URL } from "#config";
@@ -36,7 +37,7 @@ async function fetchEventsMetadata(eventIds, contract = eventsContract) {
       const eventMetadata = await fetchSingleEventMetadata(eventId, contract);
       eventsMetadata.push(eventMetadata.data);
     } catch (error) {
-      return { error };
+      throw error;
     }
   }
 
@@ -53,7 +54,7 @@ async function fetchSingleEventMetadata(eventId, contract = eventsContract) {
     eventMetadata.data.cid = eventUri;
     return eventMetadata;
   } catch (error) {
-    return { error };
+    throw error;
   }
 }
 
