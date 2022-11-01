0105767721d4fc32ff7954c19514929d0247e2a9
Vladikolev0321 <vladi.kolev2003@gmail.com>
2022-11-01 16:14:54 +0200

    fix: Fix conflict

--- test/organizer.test.js
+++ test/organizer.test.js
@@ -156,8 +156,9 @@ describe("Organizer tests", function () {
     await tx.wait();
 
     const members = await getEventMembers(tokenId, eventFacet);
+    const expectedMemberIndex = 5;
 
-    expect(members[1].account).to.equal(address);
-    expect(members[1].role).to.equal(CASHIER_ROLE);
+    expect(members[expectedMemberIndex].account).to.equal(address);
+    expect(members[expectedMemberIndex].role).to.equal(CASHIER_ROLE);
   });
 });
