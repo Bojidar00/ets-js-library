7ef5b5ecce627b82d1ce8505e51c9ba872c1517b
Vladikolev0321 <vladi.kolev2003@gmail.com>
2022-11-03 12:05:30 +0200

    feat: Add utils test file

---
+++ test/utils.js
@@ -0,0 +1,34 @@
+import eventSchema from "../config/EventFacet.json" assert { type: "json" };
+import { deployEventDiamond } from "../tasks/deployEventDiamond.js";
+import fetch from "@web-std/fetch";
+import { createEvent } from "../src/index.js";
+import { mockedMetadata, NFT_STORAGE_API_KEY } from "./config.js";
+
+async function testSetUp(diamondAddress, eventFacet, imageBlob, signers, wallet) {
+  diamondAddress = await deployEventDiamond();
+  eventFacet = await ethers.getContractAt(eventSchema.abi, diamondAddress);
+  const image = await fetch("https://www.blackseachain.com/assets/img/hero-section/hero-image-compressed.png");
+  imageBlob = await image.blob();
+  signers = await ethers.getSigners();
+  wallet = signers[0];
+  return { diamondAddress, eventFacet, imageBlob, signers, wallet };
+}
+
+async function mockedCreateEvent(maxTicketPerClient, startDate, endDate, eventFacet, wallet, tokenId) {
+  const populatedTx = await createEvent(
+    NFT_STORAGE_API_KEY,
+    mockedMetadata,
+    { maxTicketPerClient, startDate, endDate },
+    eventFacet,
+  );
+
+  const eventTx = await wallet.sendTransaction(populatedTx);
+  const eventTxResponse = await eventTx.wait();
+  const tokenIdInHex = eventTxResponse.logs[0].data.slice(2, 66); // buddy ignore:line
+  const radix = 16;
+  tokenId = parseInt(tokenIdInHex, radix);
+  console.log("New event: ", tokenId);
+  return tokenId;
+}
+
+export { testSetUp, mockedCreateEvent };
